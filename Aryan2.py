409bbbb880e86c5c510adc43a3259ca849efea92
aryankarumanchi <142139251+aryankarumanchi@users.noreply.github.com>
2023-08-13 12:15:33 +0530

    Add files via upload

---
+++ Aryan2.py
@@ -0,0 +1,13 @@
+number = int(input("Enter your luky number:"))
+print("Input number is: ", number)
+
+if (number%2) == 1:
+    print("Your favourite number is odd! ")
+else:
+    print("Your favourite number is even! ")
+
+while number >= 0:
+    print("My name is Aryan Karumanchi AK-->")
+    number = number - 1
+
+print("Number is", number)
